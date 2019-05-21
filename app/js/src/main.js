599f90331c76a3e98c878e7b96d244735899d094
thiago.campos <thiago@thiagocanudo.com.br>
2019-05-21 18:43:23 -0300

    Inclusão de plugins

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -1,3 +1,28 @@
 $(document).on('ready', function() {
-  var $test = $('#test'); 
+    console.log('jQuery load!!!');
+	var $element = $('input[type="range"]');
+	$element
+	  .rangeslider({
+	    polyfill: false,
+	    onInit: function() {
+	      var $handle = $('.rangeslider__handle', this.$range);
+	      updateHandle($handle[0], this.value);
+	    }
+	  })
+	  .on('input', function(e) {
+	    var $handle = $('.rangeslider__handle', e.target.nextSibling);
+	    updateHandle($handle[0], this.value);
+	  });
+
+	function updateHandle(el, val) {
+	  el.textContent = val;
+	}
+
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    mobileFirst: true
+  });
 });
