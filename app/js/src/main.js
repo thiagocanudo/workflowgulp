a18ce3d80e55c76ca70e49651b1dfea38ff7ed72
thiago.campos <thiago@thiagocanudo.com.br>
2019-06-14 17:57:53 -0300

    Evoluções Workflow

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -1,28 +1,10 @@
 $(document).on('ready', function() {
     console.log('jQuery load!!!');
-	var $element = $('input[type="range"]');
-	$element
-	  .rangeslider({
-	    polyfill: false,
-	    onInit: function() {
-	      var $handle = $('.rangeslider__handle', this.$range);
-	      updateHandle($handle[0], this.value);
-	    }
-	  })
-	  .on('input', function(e) {
-	    var $handle = $('.rangeslider__handle', e.target.nextSibling);
-	    updateHandle($handle[0], this.value);
-	  });
-
-	function updateHandle(el, val) {
-	  el.textContent = val;
-	}
-
   $(".regular").slick({
     dots: true,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     mobileFirst: true
   });
 });
